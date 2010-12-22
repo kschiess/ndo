71e903b8abf6c571c080a7cc8cc1e159064e2387
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2010-12-22 11:59:26 +0100

    + Yihaa! working prototype
    procrastinate makes it ridiculously easy to multiprocess. Encapsulation for the win.
    
    after this puppet ...
    another puppet inside
    encapsulated goodness

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 
-gem 'procrastinate'
+gem 'open4',          '~> 1.0.1'
+gem 'procrastinate',  '~> 0.2.0'
 
 group :development do
   gem 'rspec'
--- lib/ndo/multi_command.rb
+++ lib/ndo/multi_command.rb
@@ -1,8 +1,13 @@
 
+require 'ndo/host'
+require 'procrastinate'
+
 # A class to execute a command on a list of hosts in parallel; allows access
 # to results and is thus a) multi threaded and b) Ruby 1.9.2 only. 
 #
 class Ndo::MultiCommand
+  include Procrastinate
+
   attr_reader :command
   attr_reader :hosts
   
@@ -10,10 +15,36 @@ class Ndo::MultiCommand
     @command  = command
     @hosts    = hosts
   end
-
+  
   # Runs the command on all hosts. Returns a result collection. 
   #
   def run
-    Ndo::Results.acquire(command, hosts)
+    scheduler = Scheduler.start(SpawnStrategy::Throttled.new(5))
+    proxy = scheduler.create_proxy(self)
+
+    Results.new.tap { |results| 
+      hosts.each { |host|
+        results.store host, proxy.run_for_host(host)
+      }}
+  ensure
+    scheduler.shutdown
+  end
+  
+  class Results
+    def initialize
+      @map = Hash.new
+    end
+    
+    def [](host)
+      @map[host].value
+    end
+    
+    def store(host, future)
+      @map.store host, future
+    end
+  end
+  
+  def run_for_host(host)
+    Ndo::Host.new(host).run(@command).first
   end
 end
