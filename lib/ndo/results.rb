73712d73ac09aacab2ae36934dbcdfff23e21503
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-09-13 11:30:08 +0200

    + improves error handling
    Running commands on hosts should never raise an exception, unless an internal error happens. After a long discussion, we decide to let that exception bubble up to the surface.

--- lib/ndo/results.rb
+++ lib/ndo/results.rb
@@ -5,13 +5,13 @@ class Ndo::Results
   end
   
   def [](host)
-    begin 
-      val = @map[host].value
-      # Swallow exception returns.
-      val.instance_of?(String) ? val : nil
-    rescue Procrastinate::ChildDeath
-      nil
-    end
+    # raise ArgumentError unless @map.has_key?(host)
+    val = @map[host].value
+    
+    fail "INTERNAL ERROR: Child process raised an exception: #{val}." \
+      unless val.kind_of?(String)
+    
+    val
   end
   
   include Enumerable
@@ -19,9 +19,8 @@ class Ndo::Results
     @map.each { |host, _| 
       yield host, self[host] }
   end
-  
+    
   def store(host, future)
     @map.store host, future
   end
 end
-
